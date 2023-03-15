3751f201b40f32bdcfc26775a30864f0176fac78
Piers Gillingham <gillingham@sai-net.jp>
2023-03-15 16:57:21 +0900

    リポジトリクエリーをfindOneBY関数に変更

--- Repository/TwoFactorAuthTypeRepository.php
+++ Repository/TwoFactorAuthTypeRepository.php
@@ -41,16 +41,7 @@ class TwoFactorAuthTypeRepository extends AbstractRepository
      */
     public function findOne()
     {
-        $qb = $this->createQueryBuilder('c')->setMaxResults(1)->select('c');
-
-        // 実行
-        $result = null;
-        $results = $qb->getQuery()->getResult();
-        if (!is_null($results) && count($results) > 0) {
-            $result = $results[0];
-        }
-
-        return $result;
+        return $this->findOneBy([], ['id' => 'DESC']);
     }
 
 }
