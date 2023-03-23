5cfefb186d9cb33d9a80b7cff562c7a6f984654e
Piers Gillingham <gillingham@sai-net.jp>
2023-03-23 10:22:43 +0900

    二段階認証のプラグイン（APPorSMS）がない状態であれば、二段階認証項目の非表示状態になる https://github.com/EC-CUBE/TwoFactorAuthCustomer42/issues/33

--- Event.php
+++ Event.php
@@ -13,7 +13,10 @@
 
 namespace Plugin\TwoFactorAuthCustomer42;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Eccube\Event\TemplateEvent;
+use Eccube\Repository\BaseInfoRepository;
+use Plugin\TwoFactorAuthCustomer42\Repository\TwoFactorAuthTypeRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -22,12 +25,33 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class Event implements EventSubscriberInterface
 {
     /**
+     * @var \Eccube\Entity\BaseInfo
+     */
+    private \Eccube\Entity\BaseInfo $BaseInfo;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * @var bool
+     */
+    private bool $hasActiveAuthType;
+
+    /**
      * Event constructor.
      *
      * @throws \Exception
      */
-    public function __construct()
-    {
+    public function __construct(
+        BaseInfoRepository $baseInfoRepository,
+        TwoFactorAuthTypeRepository $twoFactorAuthTypeRepository,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->BaseInfo = $baseInfoRepository->get();
+        $this->hasActiveAuthType = $twoFactorAuthTypeRepository->findOneBy(['isDisabled' => false]) !== null;
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents(): array
@@ -46,13 +70,15 @@ class Event implements EventSubscriberInterface
      */
     public function onRenderAdminShopSettingEdit(TemplateEvent $event)
     {
-        // add twig
+        // add 本人確認認証 twig
         $twig = 'TwoFactorAuthCustomer42/Resource/template/admin/shop_edit_sms.twig';
         $event->addSnippet($twig);
 
-        // add twig
-        $twig = 'TwoFactorAuthCustomer42/Resource/template/admin/shop_edit_tfa.twig';
-        $event->addSnippet($twig);
+        if ($this->hasActiveAuthType) {
+            // add ２段階認証設定 twig
+            $twig = 'TwoFactorAuthCustomer42/Resource/template/admin/shop_edit_tfa.twig';
+            $event->addSnippet($twig);
+        }
     }
 
     /**
